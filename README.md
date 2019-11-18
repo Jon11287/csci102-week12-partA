d6acce63bb1346e779bca1953918109c0bd4de1e
Jonathan Arroyo <jjarroyo.00@gmail.com>
2019-11-18 14:03:32 -0700

    Updated README and headers with proper information

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # csci102-week12-partA
+# Incremental Build Model
+# Jonathan Arroyo
+# CSCI 102 - Section E
+# Week 12 - Part A
--- Week12-utility.py
+++ Week12-utility.py
@@ -1,3 +1,4 @@
+# https://github.com/Jon11287/csci102-week12-partA.git
 # Jonathan Arroyo
 # CSCI 102 - Section E
 # Week 12 - Part A
